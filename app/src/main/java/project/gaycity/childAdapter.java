29673306cbb995fce5e7998593d7dce687507931
fahnini <fahnini@wwu.edu>
2021-01-18 13:34:44 -0800

    started social fragment

---
+++ app/src/main/java/project/gaycity/childAdapter.java
@@ -0,0 +1,59 @@
+package project.gaycity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import project.gaycity.expandableRecyclerView.ExpandableRecyclerViewAdapter;
+import project.gaycity.expandableRecyclerView.models.ExpandableGroup;
+import project.gaycity.ui.childViewHolder;
+import project.gaycity.ui.header;
+import project.gaycity.ui.parentViewHolder;
+import project.gaycity.ui.subHeader;
+
+public class childAdapter extends ExpandableRecyclerViewAdapter<parentViewHolder, childViewHolder>{
+    private FragmentManager fm;
+    private RecyclerView recyclerView;
+    private DrawerLayout drawer;
+
+    public childAdapter(List<? extends ExpandableGroup> groups, FragmentManager fm, RecyclerView recyclerView, DrawerLayout drawer){
+        super(groups);
+        this.fm = fm;
+        this.recyclerView = recyclerView;
+        this.drawer = drawer;
+    }
+
+    @Override
+    public parentViewHolder onCreateGroupViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_group_header,parent,false);
+        return new parentViewHolder(v);
+    }
+
+    @Override
+    public childViewHolder onCreateChildViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_group_child,parent,false);
+        return new childViewHolder(v);
+    }
+
+    @Override
+    public void onBindChildViewHolder(childViewHolder holder, int flatPosition, ExpandableGroup group, int childIndex) {
+        final subHeader model = (subHeader) group.getItems().get(childIndex);
+        holder.bind(model,fm,drawer);
+    }
+
+    @Override
+    public void onBindGroupViewHolder(parentViewHolder holder, int flatPosition, ExpandableGroup group) {
+        final header model = (header) group;
+
+        holder.bind(model,fm,drawer);
+    }
+
+
+
+}
